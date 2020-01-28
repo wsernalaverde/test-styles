b5def26a1a8ae2b8a3ed1531fe2b54003a8caa8a
WSerna <wserna@tuya.com.co>
2020-01-28 15:30:37 -0500

    cambio

--- docs/custom-vex.js
+++ docs/custom-vex.js
@@ -5,5 +5,5 @@ $(document).ready(() => {
         let containerPoints = document.querySelector('.PCO-info')
         console.log(containerPoints)
         containerPoints.innerHTML = 'Tu Triple Puntaje se verá reflejado a los 3 días después de tu compra y te llegará al correo electrónico.'
-    }, 10000)
+    }, 20000)
 })
