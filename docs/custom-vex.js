a3953f2c7614b67c6c14c572f976d8a9d89b0fea
WSerna <wserna@tuya.com.co>
2020-01-29 08:52:52 -0500

    cambio

--- docs/custom-vex.js
+++ docs/custom-vex.js
@@ -4,8 +4,13 @@ $(document).ready(() => {
     setTimeout(() => {
         let containerPoints = document.querySelector('.PCO-info')
         if (containerPoints) containerPoints.innerHTML = '<p><strong>Tu Triple Puntaje</strong> se verá reflejado a los 3 días después de tu compra y te llegará al correo electrónico.</p>'
-
-        let spanTarjeta = document.querySelector('#ctl00_ctl00_NetSiteContentPlaceHolder_NetFulfillmentContentPlaceHolder_lblCCTitle')
-        if (spanTarjeta) spanTarjeta.innerHTML = 'TARJETA ÉXITO o CARULLA - MÁSTER CARD <br> <a href="https://www.tuya.com.co/tarjetas" target="_blank" style="color:#6979F8;">¿Aún no la tienes? Adquiérela aquí</a>'
     }, 5000)
+
+    setTimeout(() => {        
+        let spanTarjeta = document.querySelector('.CFTInfo #ctl00_ctl00_NetSiteContentPlaceHolder_NetFulfillmentContentPlaceHolder_lblCCTitle')
+        if (spanTarjeta) spanTarjeta.innerHTML = 'TARJETA ÉXITO o CARULLA - MÁSTER CARD <br> <a href="https://www.tuya.com.co/tarjetas" target="_blank" style="color: #6979F8 !important;font-size: 13px;margin-left: 25px;">¿Aún no la tienes? Adquiérela aquí</a>'
+
+        let spanTarjeta = document.querySelector('.divCFTInfo #ctl00_ctl00_NetSiteContentPlaceHolder_NetFulfillmentContentPlaceHolder_lblCCTitle')
+        if (spanTarjeta) spanTarjeta.innerHTML = 'TARJETA ÉXITO o CARULLA - MÁSTER CARD'
+    }, 3000)
 })
