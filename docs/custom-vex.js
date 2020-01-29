d45140f0d5e7082dbcb0f18a86f3fdc5226f933d
WSerna <wserna@tuya.com.co>
2020-01-29 08:44:07 -0500

    cambio

--- docs/custom-vex.js
+++ docs/custom-vex.js
@@ -6,6 +6,6 @@ $(document).ready(() => {
         if (containerPoints) containerPoints.innerHTML = '<p><strong>Tu Triple Puntaje</strong> se verá reflejado a los 3 días después de tu compra y te llegará al correo electrónico.</p>'
 
         let spanTarjeta = document.querySelector('#ctl00_ctl00_NetSiteContentPlaceHolder_NetFulfillmentContentPlaceHolder_lblCCTitle')
-        spanTarjeta.innerHTML = 'TARJETA ÉXITO o CARULLA - MÁSTER CARD <br> <a href="https://www.tuya.com.co/tarjetas" target="_blank" style="color:#6979F8;">¿Aún no la tienes? Adquiérela aquí</a>'
+        if (spanTarjeta) spanTarjeta.innerHTML = 'TARJETA ÉXITO o CARULLA - MÁSTER CARD <br> <a href="https://www.tuya.com.co/tarjetas" target="_blank" style="color:#6979F8;">¿Aún no la tienes? Adquiérela aquí</a>'
     }, 5000)
 })
